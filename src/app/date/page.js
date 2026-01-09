661e05d1d97187c77333efafcd3ca813a94db7b6
Jana Del Rosario <delrosariojana9@gmail.com>
2026-01-10 01:19:49 +0800

    updated the style of date page

--- src/app/date/page.js
+++ src/app/date/page.js
@@ -7,38 +7,38 @@ import { IoFastFoodOutline } from "react-icons/io5";
 const events = [
   { 
     time: "10:00 AM", 
-    title: "The Beginning",
-    activity: "Meet up at Blumentritt station.", 
+    title: "The Beginning | Blumentritt Station",
+    activity: "I shall meet you at Blumentritt station so that we can transport on a Mabini jeep together xd.", 
     icon: <MdAccessTime className="h-6 w-6 text-white" />
   },
   { 
     time: "11:00 AM", 
-    title: "The Feast",
-    activity: "We thank the Lord first for the 22 months we've spent together.", 
+    title: "The Feast at the Bay Area",
+    activity: "We thank the Lord first for the 22 months we've been able to spend together filled with love and security.", 
     icon: <MdOutlineChurch className="h-6 w-6 text-white" />
   },
   { 
     time: "01:00 PM", 
     title: "Lunch Break First!",
-    activity: "Eat at a near by restaurant in PICC to recharge.", 
+    activity: "We will eat at a near by restaurant in PICC to recharge before we set-off to BGC!", 
     icon: <IoFastFoodOutline className="h-6 w-6 text-white" />
   },
   { 
     time: "03:00 PM", 
     title: "The Mind Museum",
-    activity: "Enjoy the wonders of science and technology together in BGC.", 
+    activity: "We shall enjoy the wonders of science and technology together in BGC.", 
     icon: <MdPsychology className="h-6 w-6 text-white" />
   },
   { 
     time: "06:00 PM", 
-    title: "Marugame Udon",
-    activity: "Before going home, let's share a warm bowl of udon!", 
+    title: "Hold On! Dinner First...",
+    activity: "Before going home, let's share a warm hot meal together before we part ways T_T.", 
     icon: <CiBowlNoodles className="h-6 w-6 text-white" />
   },
   { 
     time: "07:00 PM", 
-    title: "The End!",
-    activity: "We head back home with our hearts full and happy hehe xd.", 
+    title: "The End! I'm going to miss you...",
+    activity: "We'lltravel back and head home with our hearts full and happy hehe xd.", 
     icon: <FaRegSadCry className="h-6 w-6 text-white" />
   },
 ];
@@ -58,7 +58,7 @@ export default function DateTimeline() {
 
         <div className="relative text-gray-700 antialiased text-sm font-semibold">
           
-          <div className="hidden sm:block w-1 bg-pink-300 absolute h-full left-1/2 transform -translate-x-1/2"></div>
+          <div className="hidden sm:block w-1 bg-pink-300/60 absolute h-full left-1/2 transform -translate-x-1/2"></div>
 
           {events.map((event, index) => (
             <div key={index} className="mt-6 sm:mt-0 sm:mb-12">
@@ -67,8 +67,8 @@ export default function DateTimeline() {
                 <div className={`flex w-full mx-auto items-center ${index % 2 === 0 ? "justify-start" : "justify-end"}`}>
                   <div className={`w-full sm:w-1/2 ${index % 2 === 0 ? "sm:pr-8" : "sm:pl-8"}`}>
                     
-                    <div className="p-6 w bg-white/90 rounded-xl shadow-lg hover:scale-110 transition-transform duration-300 border-t-4 border-pink-400 relative">
-                      <h3 className="text-pink-500 font-bold text-lg mb-1">{event.time}</h3>
+                    <div className="p-6 w bg-white/80 rounded-xl shadow-lg hover:scale-110 transition-transform duration-300 border-t-4 border-pink-400 relative">
+                      <h3 className="text-pink-400 font-bold text-lg mb-1">{event.time}</h3>
                       <h4 className="text-gray-900 text-[36px] font-bold text-shadow-lg font-tangerine mb-2">{event.title}</h4>
                       <p className="text-gray-600 font-serif leading-relaxed">
                         {event.activity}
